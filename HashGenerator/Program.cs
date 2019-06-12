183b8b439d22dcb4df35a26ccf56e85b9e3f0013
Ian <i.liceranzu@inizias.es>
2019-06-12 13:54:33 +0200

    Agregar archivos de proyecto.

---
+++ HashGenerator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HashGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Account mail:");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Password:");
+            string password = Console.ReadLine();
+
+            string hash = Hasher.GenerateIdentityV3Hash(password);
+            Console.WriteLine("Generated hash: " + hash);
+
+            bool verificado = Hasher.VerifyIdentityV3Hash(password, hash);
+            if (!verificado)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("ERROR");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Hash verified: " + verificado);
+
+                var logPath = "Claves.txt";
+
+                var logWriter = System.IO.File.AppendText(logPath);
+                logWriter.WriteLine(string.Format("Email: {0}   Password: {1}   Hash: {2}", email, password, hash));
+                logWriter.Dispose();
+            }
+        }
+    }
+}
